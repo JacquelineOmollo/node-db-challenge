0831f9ec484007947171bb73b2d089eec8c11de5
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-11-24 22:26:20 -0500

    migrations done

--- data/migrations/20191124213105_projects.js
+++ data/migrations/20191124213105_projects.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects", tbl => {
+    tbl.increments();
+    tbl.text("project_name", 150).notNullable();
+    tbl.text("project_description", 250);
+    tbl
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(0);
+  });
 };
-
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
--- data/migrations/20191124213354_tasks.js
+++ data/migrations/20191124213354_tasks.js
@@ -1,8 +1,23 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("tasks", tbl => {
+    tbl.increments();
+    tbl.text("task_description", 250).notNullable();
+    tbl.text("task_notes", 350);
+    tbl
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(0);
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("tasks");
 };
